6c749c2d703a0c0d584f20ef96b87f6dabe1fd29
unfencedentity <stoicam.lucian@gmail.com>
2025-11-15 17:30:18 +0100

    chore(terraform): ignore local state and tfvars

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,23 +1,7 @@
-############################################################
-# Terraform & Azure Provider Configuration
-# This file configures:
-# - The Terraform CLI version required
-# - The AzureRM provider (Azure Resource Manager)
-# - Default authentication via Azure CLI
-############################################################
-
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.88.0"
-    }
-  }
-}
-
-# Azure provider configuration (using Azure CLI authentication)
 provider "azurerm" {
   features {}
+
+  # Disable automatic Resource Provider registration.
+  # We will register only what we need manually via Azure CLI.
+  skip_provider_registration = true
 }
